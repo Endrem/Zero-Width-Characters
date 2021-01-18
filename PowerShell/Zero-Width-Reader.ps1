706c207629b67aebc70e5870313c8d2593d14e24
Endrem <github@endrem.com>
2021-01-17 19:56:08 -0500

    Formatting Changes

--- PowerShell/Zero-Width-Reader.ps1
+++ PowerShell/Zero-Width-Reader.ps1
@@ -1,14 +1,20 @@
-﻿#ZeroWidth file containing the content that will be converted to plaintext. This file should be placed within the same folder as the script.
+﻿# ZeroWidth file containing the content that will be converted to plaintext.
+# This file should be placed within the same folder as the script.
 $InputFile = Get-Content -Path ($PSScriptRoot + "\ToZeroWidth.txt")
-#Replace the zero-width characters with binary ones.
-#8203 (0x200B) = Zero-Width Space       = " "
-#8204 (0x200C) = Zero-Width Non-Joiner  = "0"
-#8205 (0x200D) = Zero-Width Joiner      = "1"
-#Trim the double quotation marks on the start and end of the string.  These were placed from the Zero-Width-Writer script for easier identification.
+
+# Replace the zero-width characters with binary ones.
+# 8203 (0x200B) = Zero-Width Space       = " "
+# 8204 (0x200C) = Zero-Width Non-Joiner  = "0"
+# 8205 (0x200D) = Zero-Width Joiner      = "1"
+# Trim the double quotation marks on the start and end of the string.
+# These were placed from the Zero-Width-Writer script for easier identification.
 $InputZeroWidth = ($InputFile).Replace([char]8203," ").Replace([char]8204,"0").Replace([char]8205,"1").Replace('"',"")
-#Convert the binary bytes into decimal.
+
+# Convert the binary bytes into decimal.
 $InputDecimal = $InputZeroWidth -split " " | ForEach-Object{[System.Convert]::ToInt32($_,2)}
-#Convert the decimal to text.
+
+# Convert the decimal to text.
 $InputString = [System.Text.Encoding]::Default.GetString($InputDecimal)
-#Save the plaintext contents to our output file.
+
+# Save the plaintext contents to our output file.
 Set-Content -Path ($PSScriptRoot + "\FromZeroWidth.txt") -Value $InputString
--- PowerShell/Zero-Width-Writer.ps1
+++ PowerShell/Zero-Width-Writer.ps1
@@ -1,12 +1,17 @@
-﻿#Plaintext file containing the content that will be converted to zero-width. This file should be placed within the same folder as the script.
+﻿# Plaintext file containing the content that will be converted to zero-width.
+# This file should be placed within the same folder as the script.
 $InputFile = Get-Content -Path ($PSScriptRoot + "\Plaintext.txt")
-#Read the InputFile and convert the text to binary.
+
+# Read the InputFile and convert the text to binary.
 $InputBinary = [System.Text.Encoding]::Default.GetBytes($InputFile) | ForEach-Object{[System.Convert]::ToString($_,2).PadLeft(8,'0')}
-#Replace the binary characters with zero-width ones.
-#8203 (0x200B) = Zero-Width Space       = " "
-#8204 (0x200C) = Zero-Width Non-Joiner  = "0"
-#8205 (0x200D) = Zero-Width Joiner      = "1"
-#Add double quotation marks on the start and end of the string.  These are placed in the script for easier identification.
+
+# Replace the binary characters with zero-width ones.
+# 8203 (0x200B) = Zero-Width Space       = " "
+# 8204 (0x200C) = Zero-Width Non-Joiner  = "0"
+# 8205 (0x200D) = Zero-Width Joiner      = "1"
+# Add double quotation marks on the start and end of the string.
+# These are placed in the script for easier identification.
 $InputZeroWidth = ('"'+$InputBinary+'"').Replace(" ",[char]8203).Replace("0",[char]8204).Replace("1",[char]8205)
-#Save the zero-width contents to our output file.
+
+# Save the zero-width contents to our output file.
 Set-Content -Path ($PSScriptRoot + "\ToZeroWidth.txt") -Value $InputZeroWidth
